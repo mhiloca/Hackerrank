147b8d6a82bc0fa089b9df7be40e69cfaac7b5b8
Mhirley Lopes <mhiloca@gmail.com>
2019-10-22 16:02:55 -0300

    tuple easy exercise

---
+++ tuples_1.py
@@ -0,0 +1,15 @@
+"""
+Task
+Given an integer, n, and n space-separated integers as input, create a tuple, t, of those n integers. Then compute and print the result of .
+Note: hash(t) is one of the functions in the __builtins__ module, so it need not be imported.
+Input Format
+The first line contains an integer, n, denoting the number of elements in the tuple.
+The second line contains n space-separated integers describing the elements in tuple .
+Output Format
+Print the result of .
+"""
+n = int(input())
+integer_list = (map(int, input().split()))
+t = tuple(integer_list)
+print(hash(t))
+
